b6032eb47a0272e03443de493406c05497ce0035
d4vc-198 <contactodiegoavc@gmail.com>
2022-12-08 17:46:27 -0600

    _strtok.c & _which.c update

--- _strtok.c
+++ _strtok.c
@@ -34,10 +34,21 @@ int t_size(char *str, char delm)
 	while (str[i] != '\0')
 	{
 		if ((str[i] == delm) && (str[i + 1] != delm))
+		{
 			num_delm++;
+			if (num_delm == 1 && str[i + 1] == '\0')
+				num_delm--;
+		}
 		i++;
 	}
-	return (num_del);
+	return (num_delm);
+}
+
+char *ignore_delm(char *str, char delm)
+{
+	while (*str == delm)
+		str++;
+	return (str);
 }
 
 /**
@@ -49,10 +60,12 @@ int t_size(char *str, char delm)
 
 char **_strtok(char *str, char *delm)
 {
-	int buffsize = 0, p = 0, si = 0, i = 0, len = 0, se = 0;
-	char **toks, d_ch;
+	int buffsize = 0, p = 0, si = 0, i = 0, len = 0, se = 0, t = 0;
+	char **toks = NULL, d_ch;
 
 	d_ch = delm[0];
+
+	str = ignore_delm(str, d_ch);
 
 	buffsize = t_size(str, d_ch);
 	toks = malloc(sizeof(char *) * (buffsize + 2));
@@ -64,25 +77,30 @@ char **_strtok(char *str, char *delm)
 		se++;
 	while (si < se)
 	{
-		len = t_strlen(str, si, d_ch);
-		toks[p] = malloc(sizeof(char) * (len + 1));
-
-		if (toks[p] == NULL)
-			return (NULL);
-		i = 0;
-		while ((str[si] != d_ch) &&
-		       (str[si] != '\0'))
+		if (str[si] != d_ch)
 		{
-			toks[p][i] = str[si];
-			i++;
-			si++;
+			len = t_strlen(str, si, d_ch);
+			toks[p] = malloc(sizeof(char) * (len + 1));
+			if (toks[p] == NULL)
+				return (NULL);
+			i = 0;
+			while ((str[si] != d_ch) &&
+			       (str[si] != '\0'))
+			{
+				toks[p][i] = str[si];
+				i++;
+				si++;
+			}
+			toks[p][i] = '\0';
+			t++;
 		}
-		toks[p][i] = '\0';
 		if (si < se && str[si + 1] != d_ch)
 			p++;
 		si++;
 	}
 	p++;
+	if (buffsize == 0) /*check if there is only 1 command*/
+		p = 1;
 	toks[p] = NULL;
 	return (toks);
 }
--- _which.c
+++ _which.c
@@ -9,12 +9,38 @@
 
 char *_which(char *str, char **env)
 {
-	char *pth, *cat, **toks;
-	int i = 0, f = 0;
+	char *pth, *cat = NULL, **toks;
+	int i = 0, f = 0, t = 0;
 
 	pth = get_env("PATH", env);
+	i = 1;
+	while (pth[i] != '\0')
+	{
+		if (pth[i - 1] == ':')
+		{
+			if (pth[i] == ':')
+				t = 1;
+			else if (i == 1)
+				t = 1;
+		}
+		i++;
+	}
+
+	if (t == 1)
+	{
+		cat = getcwd(cat, 0);
+		cat = _strcat(cat, "/");
+		cat = _strcat(cat, str);
+		if (access(cat, F_OK) == 0)
+		{
+			free(pth);
+			return (cat);
+		}
+		free(cat);
+	}
 	toks = _strtok(pth, ":");
 	free(pth);
+	i = 0;
 	while (toks[i] != NULL)
 	{
 		cat = _strdup(toks[i]);
