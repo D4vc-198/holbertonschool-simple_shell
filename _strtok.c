838716fc13c4d7457e9fbe1d97dd0d78825d6b7f
d4vc-198 <contactodiegoavc@gmail.com>
2022-12-08 11:09:05 -0600

    _strtok.c checks malloc erros

--- _strtok.c
+++ _strtok.c
@@ -1,31 +1,51 @@
 #include "shell.h"
 
+/**
+ * t_strlen - returns token string length for mallocing
+ * @str: a token
+ * @pos: index position in user's command typed into shell
+ * @delm: delimeter (e.g. " ");
+ * Return: token length
+ */
+
 int t_strlen(char *str, int pos, char delm)
 {
-	int i = 0;
+	int len = 0;
 
-	while ((str[pos] != delm) &&
-	       (str[pos] != '\0'))
+	while ((str[pos] != delm) && (str[pos] != '\0'))
 	{
 		pos++;
-		i++;
+		len++;
 	}
-	return (i);
+	return (len);
 }
+
+/**
+ * t_size - returns number of tokens for mallocing
+ * @str: user's command typed into shell
+ * @delm: delimeter (e.g. " ");
+ * Return: number of tokens
+ */
 
 int t_size(char *str, char delm)
 {
-	int i = 0, count = 0;
+	int i = 0, num_toks = 0;
 
 	while (str[i] != '\0')
 	{
 		if ((str[i] == delm) && (str[i + 1] != delm))
-			count++;
+			num_toks++;
 		i++;
 	}
-	return (count);
+	return (num_toks);
 }
 
+/**
+ * _strok - tokenizes a string and returns an array of tokens
+ * @str: user's command typed into shell
+ * @delm: delimeter (e.g. " ");
+ * Return: an array of tokens (e.g. {"ls", "-l", "/tmp"}
+ */
 
 char **_strtok(char *str, char *delm)
 {
@@ -33,14 +53,22 @@ char **_strtok(char *str, char *delm)
 	char **toks, d_ch;
 
 	d_ch = delm[0];
+
 	buffsize = t_size(str, d_ch);
 	toks = malloc(sizeof(char *) * (buffsize + 2));
+
+	if (toks == NULL)
+		return (NULL);
+
 	while (str[se] != '\0')
 		se++;
 	while (si < se)
 	{
 		len = t_strlen(str, si, d_ch);
 		toks[p] = malloc(sizeof(char) * (len + 1));
+
+		if (toks[p] == NULL)
+			return (NULL);
 		i = 0;
 		while ((str[si] != d_ch) &&
 		       (str[si] != '\0'))
@@ -56,5 +84,5 @@ char **_strtok(char *str, char *delm)
 	}
 	p++;
 	toks[p] = NULL;
-	return toks;
+	return (toks);
 }
