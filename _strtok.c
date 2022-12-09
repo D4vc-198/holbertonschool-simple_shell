6cc6a86f342867adb92af5e1d0cee1775ac869e3
d4vc-198 <contactodiegoavc@gmail.com>
2022-12-09 15:56:09 -0600

    new functions added

--- _strtok.c
+++ _strtok.c
@@ -1,13 +1,12 @@
 #include "shell.h"
 
 /**
- * t_strlen - returns token string length for mallocing
+ * t_strlen - returns token's string length for mallocing
  * @str: a token
  * @pos: index position in user's command typed into shell
  * @delm: delimeter (e.g. " ");
  * Return: token length
  */
-
 int t_strlen(char *str, int pos, char delm)
 {
 	int len = 0;
@@ -21,12 +20,11 @@ int t_strlen(char *str, int pos, char delm)
 }
 
 /**
- * t_size - returns number of delim
+ * t_size - returns number of delim ignoring continuous delim
  * @str: user's command typed into shell
  * @delm: delimeter (e.g. " ");
  * Return: number of tokens
  */
-
 int t_size(char *str, char delm)
 {
 	int i = 0, num_delm = 0;
@@ -35,10 +33,14 @@ int t_size(char *str, char delm)
 	{
 		if ((str[i] == delm) && (str[i + 1] != delm))
 		{
+			/* handle continuous delims */
 			num_delm++;
 		}
-		if (num_delm == 1 && str[i + 1] == '\0')
+		if ((str[i] == delm) && (str[i + 1] == '\0'))
+		{
+			/*handle continuous delims after full command */
 			num_delm--;
+		}
 		i++;
 	}
 	return (num_delm);
@@ -50,7 +52,6 @@ int t_size(char *str, char delm)
  * @delm: delimiter (e.g. " ")
  * Return: new string (e.g. "    ls -l" --> "ls -l")
  */
-
 char *ignore_delm(char *str, char delm)
 {
 	while (*str == delm)
@@ -64,26 +65,23 @@ char *ignore_delm(char *str, char delm)
  * @delm: delimeter (e.g. " ");
  * Return: an array of tokens (e.g. {"ls", "-l", "/tmp"}
  */
-
 char **_strtok(char *str, char *delm)
 {
 	int buffsize = 0, p = 0, si = 0, i = 0, len = 0, se = 0, t = 0;
 	char **toks = NULL, d_ch;
 
 	d_ch = delm[0];
-
+	/* creates new version of string ignoring all delims infront*/
 	str = ignore_delm(str, d_ch);
-
+	/* malloc ptrs to store array of tokens (buffsize + 1), and NULL ptr */
 	buffsize = t_size(str, d_ch);
 	toks = malloc(sizeof(char *) * (buffsize + 2));
-
 	if (toks == NULL)
 		return (NULL);
-
-	while (str[se] != '\0')
+	while (str[se] != '\0')	/* find string ending index */
 		se++;
 	while (si < se)
-	{
+	{ /* malloc lengths for each token ptr in array */
 		if (str[si] != d_ch)
 		{
 			len = t_strlen(str, si, d_ch);
@@ -97,14 +95,16 @@ char **_strtok(char *str, char *delm)
 				i++;
 				si++;
 			}
-			toks[p][i] = '\0';
+			toks[p][i] = '\0'; /* null terminate at end*/
 			t++;
 		}
+		/* handle repeated delimeters; increment ptr after ("ls __-l")*/
 		if (si < se && (str[si + 1] != d_ch && str[si + 1] != '\0'))
 			p++;
 		si++;
 	}
 	p++;
-	toks[p] = NULL;
+/*	if (p != buffsize + 1) p = t; */
+	toks[p] = NULL; /* set last array ptr to NULL */
 	return (toks);
 }
