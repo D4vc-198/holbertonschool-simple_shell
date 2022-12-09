714bf85d2daea9180207ada9eb805342e93f23cf
d4vc-198 <contactodiegoavc@gmail.com>
2022-12-09 15:48:09 -0600

    _execve.c edit file and added

--- _execve.c
+++ _execve.c
@@ -1,8 +1,12 @@
 #include "shell.h"
 
-
+/**
+ * c_exit - frees user's typed command before exiting
+ * @str: user's typed command
+ */
 void c_exit(char **str)
 {
+	free_double_ptr(str);
 	_exit(0);
 }
 
@@ -16,6 +20,7 @@ int _execve(char **s, list_t *env)
 {
 	char *holder;
 
+	/* if access sees an existing legit full cmd path, it executes cmd */
 	if (access(s[0], F_OK) == 0)
 	{
 		if (execve(s[0], s, NULL) == -1)
@@ -24,17 +29,17 @@ int _execve(char **s, list_t *env)
 			c_exit(s);
 		}
 	}
-
+	/* else flesh out full path */
 	else
-	{
 		holder = _which(s[0], env);
-	}
+
+	/* execute command with full path */
 	if (access(holder, F_OK) != 0)
 	{
 		perror("error");
 		c_exit(s);
 	}
-	else
+	else /* if not legit command, perror and exit */
 	{
 		if (execve(holder, s, NULL) == -1)
 		{
