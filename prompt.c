a734673b885e30b9647ca850ecdf47d6309808e1
d4vc-198 <contactodiegoavc@gmail.com>
2022-12-09 16:03:39 -0600

    prompt.c update file

--- prompt.c
+++ prompt.c
@@ -1,4 +1,5 @@
 #include "shell.h"
+
 /**
  * ctrl_c - ignore Ctrl-C input and prints prompt again
  * @n: takes in int from signal
@@ -6,8 +7,7 @@
 void ctrl_c(int n)
 {
 	(void)n;
-	write(STDOUT_FILENO, "\nJavi & Diego Shell$ ", 16);
-
+	write(STDOUT_FILENO, "\nKev Mel Shell$ ", 16);
 }
 
 /**
@@ -66,12 +66,9 @@ char *ignore_space(char *str)
 
 /**
  * prompt - repeatedly prompts user and executes user's cmds if applicable
- * @ac: argument count
- * @av: argument vectors
- * @env: envrionmental variables
+ * @en: envrionmental variables
  * Return: 0 on success
  */
-
 int prompt(char **en)
 {
 	list_t *env;
@@ -82,48 +79,47 @@ int prompt(char **en)
 
 	env = env_linked_list(en);
 	do {
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "Javi Diego Shell$ ", 15);
+		if (isatty(STDIN_FILENO)) /* reprompt if in interactive shell */
+			write(STDOUT_FILENO, "Kev Mel Shell$ ", 15);
 		else
 			non_interactive(env);
-		signal(SIGINT, ctrl_c);
-		command = NULL;
+		signal(SIGINT, ctrl_c); /* makes ctrl+c not work */
+		command = NULL; /* reset command to NULL each time loop runs */
 		i = 0;
 		i = _getline(&command);
 		if (i == 0)
 		{
-			free(command);
+			free(command); /* exit with newline if in shell */
 			free_linked_list(env);
-			if (isatty(STDIN_FILENO))
+			if (isatty(STDIN_FILENO))/* handles ctrl+d properly */
 				write(STDOUT_FILENO, "\n", 1);
 			exit(0);
 		}
-
 		n_command = command;
 		command = ignore_space(command);
 		n = 0;
-		while (command[n] != '\n')
+		while (command[n] != '\n') /* replace getline's \n with \0 */
 			n++;
 		command[n] = '\0';
-		if (command[0] == '\0')
+		if (command[0] == '\0') /* reprompt if user hits enter only */
 		{
 			free(n_command);
 			continue;
 		}
-		token = NULL;
+		token = NULL; /* tokenize user's typed in command */
 		token = _strtok(command, " ");
 		if (n_command != NULL)
 			free(n_command);
-		if (built_in(token, env))
+		if (built_in(token, env)) /*checks for built*/
 			continue;
-		pid = fork();
+		pid = fork(); /* create child process to execute cmd */
 		if (pid == 0)
 			_execve(token, env);
-		else
+		else /* parent waits till child finishes & frees cmd tokens */
 		{
-			wait(status);
+			wait(&status);
 			free_double_ptr(token);
 		}
-	} while (i > 0);
+	} while (i > 0); /* keep on repeating till user exits shell */
 	return (0);
 }
